1b9aeb1766807d50eb81548f158fc8220bb2cbfc
Yarosib <yarosib54@gmail.com>
2026-06-15 17:40:20 +0700

    Добавил переключатель версий, раздельные кнопки и пароль

--- server.js
+++ server.js
@@ -6,51 +6,68 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname)));
 
-// Хранилище комнат (в памяти)
-// Структура: { roomId: { players: { socketId: {nickname, avatar} }, word: null } }
 let rooms = {};
 
 io.on('connection', (socket) => {
     console.log('Новый игрок:', socket.id);
 
-    // Вход в комнату
-    socket.on('joinRoom', ({ roomId, nickname, avatar }) => {
-        const roomExists = rooms[roomId];
-        
-        // Если комнаты нет, создаем её
-        if (!roomExists) {
-            rooms[roomId] = { players: {}, word: null };
+    // Создание комнаты
+    socket.on('createRoom', ({ roomId, nickname, avatar, password }) => {
+        if (rooms[roomId]) {
+            socket.emit('errorMsg', 'Комната с таким названием уже существует!');
+            return;
         }
+
+        rooms[roomId] = { 
+            players: {}, 
+            word: null, 
+            password: password || null 
+        };
+
+        socket.join(roomId);
+        socket.roomId = roomId;
+        rooms[roomId].players[socket.id] = { nickname, avatar };
+
+        io.to(roomId).emit('updatePlayers', rooms[roomId].players);
+        io.to(roomId).emit('gameReady');
+    });
+
+    // Вход в комнату
+    socket.on('joinRoom', ({ roomId, nickname, avatar, password }) => {
+        const room = rooms[roomId];
         
-        // Ограничение на 2 игрока для простоты
-        if (Object.keys(rooms[roomId].players).length >= 7) {
+        if (!room) {
+            socket.emit('errorMsg', 'Комната не найдена!');
+            return;
+        }
+
+        // Проверка пароля
+        if (room.password && room.password !== password) {
+            socket.emit('errorMsg', 'Неверный пароль!');
+            return;
+        }
+
+        if (Object.keys(room.players).length >= 5) {
             socket.emit('errorMsg', 'Комната переполнена!');
             return;
         }
 
         socket.join(roomId);
         socket.roomId = roomId;
-        
-        // Сохраняем данные игрока
-        rooms[roomId].players[socket.id] = { nickname, avatar };
+        room.players[socket.id] = { nickname, avatar };
 
-        // Уведомляем всех в комнате об обновлении списка игроков
-        io.to(roomId).emit('updatePlayers', rooms[roomId].players);
+        io.to(roomId).emit('updatePlayers', room.players);
         
-        // Если уже есть 2 игрока, сообщаем им, что можно начинать
-        if (Object.keys(rooms[roomId].players).length === 2) {
+        if (Object.keys(room.players).length >= 2) {
             io.to(roomId).emit('gameReady');
         }
     });
 
-    // Синхронизация рисования
+    // Рисование
     socket.on('draw', (data) => {
-        // data содержит координаты, цвет, толщину
-        // Отправляем всем в комнате, КРОМЕ того, кто рисует (чтобы не лагало у автора)
         socket.to(socket.roomId).emit('draw', data);
     });
 
-    // Очистка холста
     socket.on('clearCanvas', () => {
         socket.to(socket.roomId).emit('clearCanvas');
     });
@@ -64,7 +81,6 @@ io.on('connection', (socket) => {
             isCorrect = true;
         }
 
-        // Отправляем сообщение всем + флаг, угадал ли кто-то
         io.to(roomId).emit('chatMessage', { 
             user: rooms[roomId].players[socket.id].nickname, 
             text: msg, 
@@ -74,24 +90,21 @@ io.on('connection', (socket) => {
 
         if (isCorrect) {
             io.to(roomId).emit('winGame', currentWord);
-            rooms[roomId].word = null; // Сброс слова
+            rooms[roomId].word = null;
         }
     });
 
-    // Установка загаданного слова (делает художник)
     socket.on('setSecretWord', (word) => {
         if (rooms[socket.roomId]) {
             rooms[socket.roomId].word = word;
         }
     });
 
-    // Выход из игры
     socket.on('disconnect', () => {
         if (socket.roomId && rooms[socket.roomId]) {
             delete rooms[socket.roomId].players[socket.id];
             io.to(socket.roomId).emit('updatePlayers', rooms[socket.roomId].players);
             
-            // Если комната пуста, удаляем её
             if (Object.keys(rooms[socket.roomId].players).length === 0) {
                 delete rooms[socket.roomId];
             } else {
@@ -103,5 +116,5 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
-    console.log(`Сервер запущен на http://localhost:${PORT}`);
+    console.log(`Сервер запущен на порту ${PORT}`);
 });
